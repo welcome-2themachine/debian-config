10e3b86bbd3172572129fff8cb9d717c6bc84559
welcomw <welcome2.themachine411@gmail.com>
2023-08-05 23:27:46 -1000

    added microcode

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 
 #add more things before this
 
-chmod +x miniscripts/*
+#chmod +x miniscripts/*
 #update /apt/sources.list
-./miniscripts/sources.sh
-sudo apt update && sudo apt upgrade
+#./miniscripts/sources.sh
+#sudo apt update && sudo apt upgrade
 
 # General List
 #./user_setup.sh
+#./miniscripts/amd_cpu.sh
+#./miniscripts/intel_cpu.sh
 #./miniscripts/sshd_setup.sh
 #./miniscripts/ufw_setup.sh
 #./miniscripts/app_install.sh
@@ -27,3 +29,36 @@ sudo apt update && sudo apt upgrade
 # XPS 15 9520 List
 #./miniscripts/fingerprint.sh
 #./miniscripts/nvidia.sh
+
+while getopts "anxdh" option; do
+    case $option in
+        a)
+            #amd studd
+            echo "Installing amd drivers"
+        ;;
+        n)
+            echo "Installing nvidia drivers"
+        ;;
+        x)
+            #flag1 code
+            echo "Now running scripts for XPS 15 9520"
+        ;;
+        d)
+            #flag2 code
+            echo "Now running scripts for your custom desktop"
+        ;;
+        h)
+            #none code
+            echo "Options:"
+            echo " -x: for XPS 15 9520"
+            echo " -d: for my am4/radeon based desktop"
+            echo " -n: to install nvidia gpu drivers"
+            echo " -a: to install amd gpu drivers"
+            echo " -h: to show this menu"
+        ;;
+        \?)
+            #flag3 code
+            echo "some error"
+        ;;
+    esac
+done
