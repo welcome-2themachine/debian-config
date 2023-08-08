63fffb55dedc6ca9ff836ffbb8394bb68b4f7560
welcome-2themachine <welcome2.themachine411@gmail.com>
2023-08-07 22:12:05 -1000

    separated gnome from app_install

---
+++ miniscripts/gnome.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+sudo apt update
+
+sudo apt install \
+adwaita-icon-theme \
+boabab \
+dconf-cli \
+dconf-gsettings-backend \
+gdm3 \
+glib-networking \
+gnome-backgrounds \
+gnome-calculator \
+gnome-control-center \
+gnome-keyring \
+gnome-logs \
+gnome-menus \
+gnome-session \
+gnome-software \
+gnome-sushi \
+gnome-shell \
+gnome-system-monitor \
+gnome-text-editor \
+gnome-terminal \
+gnome-disk-utility \
+gnome-software-plugin-flatpak \
+gnome-software-plugin-snap \
+gnome-browser-connector \
+gnome-tweaks \
+gnome-text-editor \
+gnome-themes-extra \
+gnome-shell-extension-manager \
+gvfs-backends \
+gvfs-fuse \
+nautilus \
+pipewire-audio \
+sound-theme-freedesktop \
+system-config-printer-udev \
+network-manager-gnome
