9b2e26178b055116c37c8d017405618e62639a7a
welcome-2themachine <welcome2.themachine411@gmail.com>
2023-08-08 10:49:43 -1000

    switched discord.sh to dpkg

--- miniscripts/discord.sh
+++ miniscripts/discord.sh
@@ -5,5 +5,5 @@
 # License: GPL3
 sudo apt install wget
 wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
-sudo apt install discord.deb
+sudo dpkg -i discord.deb
 rm discord.deb
