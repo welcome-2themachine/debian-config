70e69507f6a6003dd556e49fca4c91542b83bca9
welcomw <welcome2.themachine411@gmail.com>
2023-08-08 23:23:42 -1000

    added openrgb miniscript

---
+++ miniscripts/openrgb.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+#sudo apt install wget
+#wget -O https://openrgb.org/releases/release_0.9/openrgb_0.9_amd64_bookworm_b5f46e3.deb openrgb.deb
+#sudo dpkg -i openrgb.deb
+#rm openrgb.deb
+
+sudo apt install git build-essential qtcreator qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libusb-1.0-0-dev libhidapi-dev pkgconf libmbedtls-dev debhelper
+
+git clone https://gitlab.com/CalcProgrammer1/OpenRGB
+cd OpenRgb
+
+qmake OpenRGB.pro
+#qmak -qt5 OpenRGB.pro
+make -j$(nproc)
+sudo make install
+
+#There's some room to trim down to only the items needed
