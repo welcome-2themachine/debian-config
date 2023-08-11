db22de8788c24b1dfc3c7b3e5fa76ab9791072d8
welcome2.themachine411 <welcome2.themachine411@gmail.com>
2023-08-10 23:10:05 -1000

    Now elegant install script

--- install.sh
+++ install.sh
@@ -5,172 +5,44 @@
 # Author: welcome-2themachine
 # License: GPL3
 
-#add more things before this
+echo "Configure install.sh by uncommenting the miniscripts you need for your device"
 
-#chmod +#x miniscripts/*
-#chmod +x user_setup.sh
+##### AMD CPU #####
+#miniscripts/amd_cpu.sh
+##### INTEL CPU #####
+#miniscripts/intel_cpu.sh
 
-#update /apt/sources.list
-###./miniscripts/sources.sh
-#sudo apt update && sudo apt upgrade
+##### AMD GPU #####
+#miniscripts/amd_gpu.sh
+##### NVIDIA GPU #####
+#miniscripts/nvidia.sh
 
-# General List
-#./miniscripts/app_install.sh
-#./miniscripts/gnome.sh
-./user_setup.sh
-#./miniscripts/plymouth.sh
-#./miniscripts/gnome-extensions.sh
-#./miniscripts/steam.sh
-#./miniscripts/parsecd.sh
-#./miniscripts/snapd.sh
-#./miniscripts/sshd_setup.sh
-#./miniscripts/ufw_setup.sh
-#./miniscripts/cloudflared.sh
-#./miniscripts/vscode.sh
-#./miniscripts/install_nordvpn.sh
-#./miniscripts/discord.sh
-#./miniscripts/obsidian.sh
-#./miniscripts/nordvpn.sh
-#./miniscripts/signal-desktop.sh
-#./miniscripts/card_reader_setup.sh
-#./miniscripts/xbox_controller_setup.sh
-# Desktop List
-###./miniscripts/amd.sh
+##### LAPTOP POWER #####
+#miniscripts/laptop_power.sh
 
-# XPS 15 9520 List
-###./miniscripts/fingerprint.sh
-###./miniscripts/nvidia.sh
-print_help()
-{
-    echo "Options:"
-    echo " -a|--amdgpu: installs amd gpu drivers"
-    echo " -n|--nvidia: install nvidia gpu drivers"
-    echo " -i|--intelcpu: installs intel cpu microcode"
-    echo " -c|--amdcpu: installs amd cpu microcode"
-    echo " -x|--xps: install script for my XPS 15 9520"
-    echo " -d|--desktop: for my am4/radeon based desktop"
-    echo "      https://pcpartpicker.com/list/2v7nzf"
-    echo " -h|--help: to show this menu"
-}
+##### General Apps #####
+#miniscripts/app_install.sh
+#miniscripts/card_reader_setup.sh
+#miniscripts/cloudflared.sh
+#miniscripts/discord.sh
+#miniscripts/fingerprint.sh
+#miniscripts/nordvpn.sh
+#miniscripts/obsidian.sh
+#miniscripts/openrgb.sh
+#miniscripts/parsecd.sh
+#miniscripts/plymouth.sh
+#miniscripts/signal-desktop.sh
+#miniscripts/snapd.sh
+#miniscripts/sources.sh
+#miniscripts/sshd_setup.sh
+#miniscripts/steam.sh
+#miniscripts/ufw_setup.sh
+#miniscripts/vscode.sh
+#miniscripts/xbox_controller_setup.sh
 
-#***********************************************************************
-# REMOVE IF YOU GET IT WORKING
-echo "this scrips is unfinished"
-exit 0
-#***********************************************************************
+##### Set Up User #####
+#user_setup.sh
 
-SHORTOPTS="anicxdh"
-LONGOPS="amdgpu,nvidia,intelcpu,amdcpu,xps,desktop,help"
-
-if $(getopt -T >/dev/null 2>&1) ; [ $? = 4 ] ; then # New longopts getopt.
-    OPTS=$(getopt -o $SHORTOPTS --long $LONGOPTS -n "$progname" -- "$@")
-else # Old classic getopt.
-    # Special handling for --help and --version on old getopt.
-    case $1 in --help) print_help ; exit 0 ;; esac
-    OPTS=$(getopt $SHORTOPTS "$@")
-fi
-
-if [ $? -ne 0 ]; then
-    echo "'$progname --help' for more information" 1>&2
-    exit 1
-fi
-
-eval set -- "$OPTS"
-# I don't entirely understand all of this ^ https://lists.gnu.org/archive/html/help-gnu-utils/2008-11/msg00002.html
-
-nvidia_gpu=false
-amd_gpu=false
-amd_cpu=false
-intel_cpu=false
-fingerprint=false
-i=1
-
-while [ $# -gt 0 ]; do
-    echo "run: $i"
-    : debug: $1
-    case $1 in
-        -a|--amdgpu)
-        #amd gpu
-            echo "Installing amd drivers"
-            amd_gpu=true
-            shift
-            ;;
-        -n|--nvidia)
-        # nvidia gpu
-            echo "Installing nvidia drivers"
-            nvidia_gpu=true
-            shift
-            ;;
-        -i|--intelcpu)
-        # intel cpu
-            echo "installing intel microcode"
-            intel_cpu=true
-            shift
-            ;;
-        -c|--amdcpu)
-        # amd cpu
-            echo "installing amd microcode"
-            amd_cpu=true
-            shift
-            ;;
-        -x|--xps)
-        # script for my xps 9520
-            echo "Now running scripts for XPS 15 9520"
-            intel_cpu=true
-            nvidia_gpu=true
-            fingerprint=true
-            shift
-            ;;
-        -d|--desktop)
-        # script for my desktop https://pcpartpicker.com/list/2v7nzf
-            echo "Now running scripts for your custom desktop"
-            amd_cpu=true
-            amd_gpu=true
-            shift
-            ;;
-        -h|--help)
-        # help
-            echo "help requested"
-            print_help
-            exit 0
-            ;;
-        --)
-            echo "i got here --"
-            echo "$1"
-            shift
-#            break
-            ;;
-        *)
-            #invalid input
-            echo "Invalid input: option processing error: $1" 1>&2
-            print_help
-            exit 1
-            ;;
-    esac
-    i=$((i+1))
-done
-
-#check for conflicts
-if $amd_cpu && $intel_cpu; then
-    echo "ERROR: Cannot specify both intel and amd cpu" 1>&2
-    exti 1
-fi
-
-if $amd_gpu && $nvidia_gpu; then
-    echo "ERROR: Cannot specify both amd and nvidia gpu" 1>&2
-    exit 1
-fi
-
-#gpu
-if $amd_gpu; then  
-    echo "amdgpu"
-fi
-if $nvidia_gpu; then    
-    echo "nvidia"
-fi
-if $amd_cpu; then
-    echo "amdcpu"
-fi
-if $intel_cpu; then
-    echo "intel"
-fi
+##### DM Install #####
+#miniscripts/gnome.sh
+#miniscripts/gnome-extensions.sh
--- miniscripts/openrgb.sh
+++ miniscripts/openrgb.sh
@@ -5,14 +5,17 @@
 #sudo dpkg -i openrgb.deb
 #rm openrgb.deb
 
-sudo apt install git build-essential qtcreator qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libusb-1.0-0-dev libhidapi-dev pkgconf libmbedtls-dev debhelper
+#sudo apt install git build-essential qtcreator qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libusb-1.0-0-dev libhidapi-dev pkgconf libmbedtls-dev debhelper
 
-git clone https://gitlab.com/CalcProgrammer1/OpenRGB
-cd OpenRgb
+#git clone https://gitlab.com/CalcProgrammer1/OpenRGB
+#cd OpenRgb
 
-qmake OpenRGB.pro
-#qmak -qt5 OpenRGB.pro
-make -j$(nproc)
-sudo make install
+#qmake OpenRGB.pro
+##qmak -qt5 OpenRGB.pro
+#make -j$(nproc)
+#sudo make install
+
+sudo dpkg -i resources/openrgb_0.9_amd64_bookworm_b5f46e3.deb
+sudo mv resoureces/i2c-dev.conf /etc/modeprobe.d/
 
 #There's some room to trim down to only the items needed
